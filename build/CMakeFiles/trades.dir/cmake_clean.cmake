c3ee55cb4eafb8811deb8bdea656612f584c887c
arushiag12 <arushiag@uchicago.edu>
2024-05-23 14:47:48 -0500

    upload source code

---
+++ build/CMakeFiles/trades.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trades.dir/driver.cpp.obj"
+  "CMakeFiles/trades.dir/driver.cpp.obj.d"
+  "libtrades.dll.a"
+  "trades.exe"
+  "trades.exe.manifest"
+  "trades.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trades.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
